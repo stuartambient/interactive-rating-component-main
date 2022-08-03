03dfb3ee3848d4a0abc327f97f46e83afc26edbe
stuartambient <sambient@gmail.com>
2022-08-02 18:22:30 -0600

    getting there

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const ratings = document.querySelectorAll('.ratings__rank');
-
+const ratingsWrapper = document.querySelector('.ratings__wrapper');
+const confirmationWrapper = document.querySelector('.confirmation__wrapper');
+const setResult = document.querySelector('.confirmation__result');
 const submitRating = document.querySelector('.ratings__submit');
 
 let rating;
 
-const removePrev = target => {
-  ratings.forEach(r => {
-    if (r.classList.contains('active') && r !== target) {
-      r.classList.remove('active');
+const submit = () => {
+  ratings.forEach(rank => {
+    if (rank.classList.contains('active')) {
+      rank.classList.remove('active');
+    }
+  });
+  ratingsWrapper.classList.add('ratings__wrapper--remove');
+  confirmationWrapper.classList.add('confirmation__wrapper--display');
+  setResult.textContent = rating;
+};
+
+const changeRank = () => {
+  const ratingsArr = [...ratings];
+
+  ratingsArr.forEach(rank => {
+    if (rank.value > rating) {
+      rank.classList.remove('active');
+    }
+    if (rank.value <= rating) {
+      rank.classList.add('active');
     }
   });
 };
 
 ratings.forEach(item => {
-  item.addEventListener('click', item => {
-    removePrev(item.target);
-    item.target.classList.toggle('active');
+  item.addEventListener('mouseover', item => {
     rating = item.target.value;
+    changeRank();
   });
+});
+
+submitRating.addEventListener('click', () => {
+  submit();
 });
